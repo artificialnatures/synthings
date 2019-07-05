008c76ce6c16bd8343974520efb751555707a05b
Aaron Westre <aaron@artificialnatures.com>
2019-07-05 18:20:53 -0500

    created synthings.csharp solution

---
+++ synthings.csharp/synthings.core.tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace synthings.core.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ synthings.csharp/synthings.core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace synthings.core
+{
+    public class Class1
+    {
+    }
+}
