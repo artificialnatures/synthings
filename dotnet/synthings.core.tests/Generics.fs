731220d04e2027aff8908387ff499bc1696d8661
Aaron Westre <aaron@artificialnatures.com>
2019-10-16 18:00:35 -0500

    marker interfaces work with record types

--- dotnet/synthings.core.tests/Generics.fs
+++ dotnet/synthings.core.tests/Generics.fs
@@ -11,8 +11,10 @@ type Word =
     interface SubjectType
 
 type Number =
-    | Number of int
+    | Number of float
     interface SubjectType
+
+type WordNumber = {Text : string; Amount : float} interface SubjectType
 
 type Change<'subject when 'subject :> SubjectType> =
     {
@@ -31,11 +33,21 @@ let ``Changes of different types`` () =
     let changes =
         [
             WordChange {Value = Word "this"}
-            NumberChange {Value = Number 1}
+            NumberChange {Value = Number 1.0}
             WordChange {Value = Word "that"}
-            NumberChange {Value = Number 2}
+            NumberChange {Value = Number 2.0}
             WordChange {Value = Word "the other"}
-            NumberChange {Value = Number 3}
+            NumberChange {Value = Number 3.0}
         ]
-    let words = List.choose (fun change -> match change with | WordChange wordChange -> Some (string wordChange.Value) | _ -> None) changes
-    Assert.Equal(3, words.Length)
+    let chooseWord change =
+        match change with
+        | WordChange wordChange -> Some (string wordChange.Value)
+        | _ -> None
+    let strings = List.choose chooseWord changes
+    let chooseNumber change =
+        match change with
+        | NumberChange numberChange -> Some (float numberChange.Value) //Why?
+        | _ -> None
+    let ints = List.choose chooseNumber changes
+    Assert.Equal(3, strings.Length)
+    Assert.Equal(3, ints.Length)
