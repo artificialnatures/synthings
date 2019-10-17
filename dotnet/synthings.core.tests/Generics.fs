7af808ba5e8397a6ecbca207191cbb26f1f3a3bc
Aaron Westre <aaron@artificialnatures.com>
2019-10-17 15:19:39 -0500

    added WordNumber

--- dotnet/synthings.core.tests/Generics.fs
+++ dotnet/synthings.core.tests/Generics.fs
@@ -2,21 +2,27 @@ module synthings.core.tests.Generics
 
 open Xunit
 
-//Like this?
+//In F# generic types can be combined with marker (empty) interfaces
+//and discriminated unions to enable bounded generic handling of
+//data types by pattern matching.
+//The logic is:
+// 1. box(box(box(data))) //each box corresponds to a layer of abstraction: data -> generic type -> discriminated union case -> marker interface type
+// 2. collect the wrapped data into a list of state changes
+// 3. match on library -> match on grouping -> match on data type -> data //unbox the data to use it on the consumer side
 
-type SubjectType = interface end
+type Subject = interface end
 
 type Word =
     | Word of string
-    interface SubjectType
+    interface Subject
 
 type Number =
     | Number of float
-    interface SubjectType
+    interface Subject
 
-type WordNumber = {Text : string; Amount : float} interface SubjectType
+type WordNumber = {Text : string; Amount : float} interface Subject
 
-type Change<'subject when 'subject :> SubjectType> =
+type Change<'subject when 'subject :> Subject> =
     {
         Value : 'subject
     }
@@ -26,10 +32,11 @@ type ChangeType = interface end
 type TestChange =
     | WordChange of Change<Word>
     | NumberChange of Change<Number>
+    | WordNumberChange of Change<WordNumber>
     interface ChangeType
 
 [<Fact>]
-let ``Changes of different types`` () =
+let ``Generic Changes`` () =
     let changes =
         [
             WordChange {Value = Word "this"}
@@ -38,16 +45,23 @@ let ``Changes of different types`` () =
             NumberChange {Value = Number 2.0}
             WordChange {Value = Word "the other"}
             NumberChange {Value = Number 3.0}
+            WordNumberChange {Value = {Text = "Word"; Amount = 10.0}}
         ]
     let chooseWord change =
         match change with
-        | WordChange wordChange -> Some (string wordChange.Value)
+        | WordChange wordChange -> Some wordChange.Value
         | _ -> None
     let strings = List.choose chooseWord changes
     let chooseNumber change =
         match change with
-        | NumberChange numberChange -> Some (float numberChange.Value) //Why?
+        | NumberChange numberChange -> Some numberChange.Value
         | _ -> None
     let ints = List.choose chooseNumber changes
+    let chooseWordNumber change =
+        match change with
+        | WordNumberChange wordNumberChange -> Some wordNumberChange.Value
+        | _ -> None
+    let wordNumbers = List.choose chooseWordNumber changes
     Assert.Equal(3, strings.Length)
     Assert.Equal(3, ints.Length)
+    Assert.Equal(1, wordNumbers.Length)
