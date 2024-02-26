933768834a0bc21f05bcdd1f809f288bd2c5a98c
Aaron Westre <aaron@artificialnatures.com>
2024-02-26 11:00:20 -0600

    Page loaded event

--- ui/maui/SynthingsMauiApplication.fs
+++ ui/maui/SynthingsMauiApplication.fs
@@ -4,10 +4,6 @@ open synthings.transmission
 //TODO: inject - application : Application<StateRepresentation>
 type SynthingsMauiApplication() as mauiApp =
     inherit Microsoft.Maui.Controls.Application()
-    //let colors = ResourceDictionary(Source=System.Uri("Resources/Styles/Colors.xaml"))
-    //do this.Resources.MergedDictionaries.Add(colors)
-    //let styles = ResourceDictionary(Source=System.Uri("Resources/Styles/Styles.xaml"))
-    //do this.Resources.MergedDictionaries.Add(styles)
     let startMessage = Microsoft.Maui.Controls.Label(
             Text="synthings",
             FontFamily="Atkinson",
@@ -18,5 +14,6 @@ type SynthingsMauiApplication() as mauiApp =
     let rootContent = Microsoft.Maui.Controls.ContentView(Content=startMessage)
     let rootPage = Microsoft.Maui.Controls.ContentPage(Content=rootContent)
     do mauiApp.MainPage <- rootPage
-    let complete _ = ()
+    let complete _ =
+        startMessage.Text <- "synthings -> loaded"
     do rootPage.Loaded.Add(complete)
