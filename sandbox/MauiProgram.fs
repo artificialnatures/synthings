b417e98d731063a115e46bc4125f6148c9cf2154
Aaron Westre <aaron@artificialnatures.com>
2024-02-28 11:08:30 -0600

    Sandbox app

---
+++ sandbox/MauiProgram.fs
@@ -0,0 +1,25 @@
+﻿namespace synthings.sandbox
+
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Maui.Hosting
+open Microsoft.Maui.Controls.Hosting
+open synthings.ui.maui
+
+type MauiProgram =
+    static member CreateMauiApp() =
+        let mauiApp =
+            MauiApp
+                .CreateBuilder()
+                .UseMauiApp<SynthingsMauiApplication>()
+        mauiApp.Services.AddSingleton(Sandbox.build ())
+        |> ignore
+        mauiApp.ConfigureFonts(fun fonts ->
+                fonts
+                    .AddFont("BerkeleyMono-Regular.ttf", "BerkeleyMono")
+                    .AddFont("BerkeleyMono-Bold.ttf", "BerkeleyMonoBold")
+                    .AddFont("BerkeleyMono-Italic.ttf", "BerkeleyMonoItalic")
+                    .AddFont("BerkeleyMono-BoldItalic.ttf", "BerkeleyMonoBoldItalic")
+                |> ignore
+            )
+            |> ignore
+        mauiApp.Build()
---
+++ sandbox/Sandbox.fs
@@ -0,0 +1,25 @@
+namespace synthings.sandbox
+
+open synthings.transmission
+
+module Sandbox =
+    let goodbyeState =
+        Node (VerticalStack, [
+            Leaf (Text "Goobye, world!")
+        ])
+    let helloState =
+        Node (ApplicationContainer, [
+            Node (Window {title = "sandbox"}, [
+                Node (VerticalStack, [
+                    Leaf (Text "Hello, world!")
+                    Leaf (Button ("OK", Replace {entityToReplace=Ancestor 1; replacement=goodbyeState}))
+                ])
+            ])
+        ])
+    let initialProposal =
+        Initialize {initialTree = helloState}
+    
+    let build () =
+        let transmission = Transmission()
+        transmission.Enqueue Identifier.empty initialProposal
+        transmission
